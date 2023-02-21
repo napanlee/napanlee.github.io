b470c1670208382171dcb9ffca41973eef8a7772
napanlee <leeg124@newschool.edu>
2023-02-21 08:58:03 -0500

    Project #1
    R-Final

--- project1fourthpage.js
+++ project1fourthpage.js
@@ -3,17 +3,13 @@ $(dropOnScale);
 function dropOnScale() {
   $('.dropme').draggable();
   $('#left, #right').droppable({
-    // handleDropEvent will provide instructions for browser when something is dropped!
     drop:handleDropEvent,
   });
 }
   
   function handleDropEvent(event,ui) {
-    // dragID variable stores draggable element ID
     var dragId = ui.draggable.attr("id");
-    // dropID variable stores droppable element ID, when draggable is dropped.
     var dropId = $("#left,#right").attr("id");
-    // Append each draggable item to text on page.
     $("#listdropped").append(dragId);
   }
 
