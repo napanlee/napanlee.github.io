110db603e7544d2f6411135bf1234ba587596a92
napanlee <leeg124@newschool.edu>
2023-04-05 19:59:51 -0400

    updatedproject2

--- project 2/collection.js
+++ project 2/collection.js
@@ -1,47 +1,38 @@
-// Function to render your items
+
 const renderItems = (collection) => {
-	// The `ul` where the items will be inserted
 	const collectionList = document.getElementById('collection');
 	
-	// Loop through each item in the collection array
+
 	collection.forEach(item => {
-		const listItem = document.createElement('li') // Make the `li`
+		const listItem = document.createElement('li')
 
-		// You can make each element inside of that…
-		const itemTitle = document.createElement('h2') // Make an `h2`
-		itemTitle.innerHTML = item.title // Put the JSON title inside
-		listItem.appendChild(itemTitle) // And add it to the `li`!
+		const itemTitle = document.createElement('h2') 
+		itemTitle.innerHTML = item.title 
+		listItem.appendChild(itemTitle) 
 
-		const itemImage = document.createElement('img') // And an image
-		itemImage.src = item.images // Set the `src` attribute from the JSON
-		listItem.appendChild(itemImage) // And add that too
-
-		// add an ID to the list item for unique styling
+		const itemImage = document.createElement('img') 
+		itemImage.src = item.images 
+		listItem.appendChild(itemImage)
 		listItem.id = item.id;
  
-		// This can get annoying, so we can use “template literals” instead
 		const itemDetails =
 			`
 				<p>${item.number}</span></p>
 
 
 			`
-		listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
+		listItem.insertAdjacentHTML('beforeend', itemDetails) 
 
-		// You can build logic from your data, too
-		// if (!item.otherAttr) { // If this is `false`
-		// 	listItem.classList.add() // Add this class to the whole `li`
-		// }
 
-		collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
+
+		collectionList.appendChild(listItem)
 	})
 }
 
 
-// Fetch gets your JSON file…
+
 fetch('collection.json')
 	.then(response => response.json())
 	.then(collection => {
-		// And passes the data to the function, above!
-		renderItems(collection) // In reverse order
+		renderItems(collection) 
 })
